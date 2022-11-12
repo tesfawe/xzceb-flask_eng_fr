cca678e88b73de0472f448e1e5d290a0356fa094
tesfawe <tesfamit03@gmail.com>
2022-11-12 04:23:48 -0500

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,12 +6,18 @@ class TestE2F(unittest.TestCase):
 
     def test_english_to_french(self):
         self.assertNotEqual(english_to_french("None"), '')
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')  
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertEqual(english_to_french('Thank you'), 'Je vous remercie')
+        self.assertEqual(english_to_french('Goodbye'), 'Au revoir')  
 
 class TestF2E(unittest.TestCase):
     """English to French tests"""
     def test_french_to_english(self):
         self.assertNotEqual(french_to_english("None"), '')
-        self.assertEqual(french_to_english('Bonjour'), 'Hello')  
+        self.assertEqual(french_to_english('Bonjour'), 'Hello') 
+        self.assertEqual(french_to_english('Je vous remercie'), 'Thank you.')
+        self.assertEqual(french_to_english('Au revoir'), 'Goodbye')  
+ 
 
-unittest.main()                
+if __name__ == '__main__':
+    unittest.main()      
